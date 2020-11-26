f92a44cc6bb20716e2f7695079167c363c1a0b4a
Balint Kovari <balintmaci@gmail.com>
2020-11-26 10:35:32 +0100

    get pointcloud messages

--- radar-test-package/src/cam.py
+++ radar-test-package/src/cam.py
@@ -5,18 +5,22 @@ import numpy as np
 import rospy
 from cv_bridge import CvBridge
 
-from sensor_msgs.msg import LaserScan, Range, Image
+from sensor_msgs.msg import LaserScan, Range, Image, PointCloud2
 from geometry_msgs.msg import PoseStamped
 from message_tools import create_setpoint_message_xyz_yaw
 
 depth_image_topic = '/camera/depth/image_raw'
+rgb_image_topic = '/camera/rgb/image_raw'
+cloud_topic = '/camera/depth/points'
 
 
 class LidarProcessor():
     def __init__(self):
         rospy.init_node('depth_image_processing')
 
-        self._depth_image_sub = rospy.Subscriber(depth_image_topic, Image, self.on_depth_image)
+        # self._depth_image_sub = rospy.Subscriber(depth_image_topic, Image, self.on_depth_image)
+        self._cloud_sub = rospy.Subscriber(cloud_topic, PointCloud2, self.on_cloud)
+
 
         # self._displacement_pub = rospy.Publisher(topic_displacement, PoseStamped, queue_size=10)
 
@@ -34,6 +38,10 @@ class LidarProcessor():
         w = img.shape[0]
         h = img.shape[1]
         return img[w/3:2*w/3,h/3:2*h/3]
+
+    def on_cloud(self, msg = PointCloud2()):
+        rospy.logout('asd')
+        rospy.logout(msg.header)
 
 
 if __name__ == "__main__":
